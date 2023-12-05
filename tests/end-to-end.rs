4c0f0fd1f4f777069b0c91266872d069c00ebc0b
vvh413 <nnenekit@gmail.com>
2023-12-05 23:47:40 +0300

    generate rangom image + selective jpeg algo test

--- tests/end-to-end.rs
+++ tests/end-to-end.rs
@@ -13,14 +13,20 @@ fn rand_string(size: usize) -> String {
     .collect()
 }
 
-fn e2e(ext: &str, image_size: (u32, u32), data_size: usize, extra: ExtraArgs) -> Result<()> {
+fn e2e(ext: &str, image_size: (u32, u32), data_size: usize, extra: ExtraArgs, rand: bool) -> Result<()> {
   println!("--- {image_size:?} {data_size} {extra:?}");
   let in_path = format!("/tmp/s739_in_{image_size:?}_{data_size}_{extra:?}.{ext}");
   let out_path = format!("/tmp/s739_out_{image_size:?}_{data_size}_{extra:?}.{ext}");
   let data = rand_string(data_size).into_bytes();
 
   println!("encoding");
-  image::DynamicImage::ImageRgb8(image::ImageBuffer::new(image_size.0, image_size.1)).save(in_path.clone())?;
+  let mut image_buffer = image::ImageBuffer::new(image_size.0, image_size.1);
+  if rand {
+    let mut rng = thread_rng();
+    image_buffer.iter_mut().for_each(|pixel| *pixel = rng.gen());
+  }
+  image::DynamicImage::ImageRgb8(image_buffer).save(in_path.clone())?;
+
   let mut encoder = new_encoder(in_path.into(), extra.clone())?;
   println!("image_size: {}", encoder.total_size());
   encoder.write_data(&data)?;
@@ -48,13 +54,13 @@ fn e2e(ext: &str, image_size: (u32, u32), data_size: usize, extra: ExtraArgs) ->
 
 #[test]
 fn png_default() -> Result<()> {
-  e2e("png", (128, 128), 128, ExtraArgs::default())?;
+  e2e("png", (128, 128), 128, ExtraArgs::default(), false)?;
   Ok(())
 }
 
 #[test]
 fn jpeg_default() -> Result<()> {
-  e2e("jpg", (128, 128), 128, ExtraArgs::default())?;
+  e2e("jpg", (128, 128), 128, ExtraArgs::default(), false)?;
   Ok(())
 }
 
@@ -68,6 +74,7 @@ fn png_key() -> Result<()> {
       key: Some("some key".to_string()),
       ..Default::default()
     },
+    false,
   )?;
   Ok(())
 }
@@ -82,6 +89,7 @@ fn jpeg_key() -> Result<()> {
       key: Some("some key".to_string()),
       ..Default::default()
     },
+    false,
   )?;
   Ok(())
 }
@@ -97,6 +105,7 @@ fn png_depth() -> Result<()> {
         depth,
         ..Default::default()
       },
+      false,
     )?;
   }
   Ok(())
@@ -113,6 +122,7 @@ fn jpeg_depth() -> Result<()> {
         depth,
         ..Default::default()
       },
+      false,
     )?;
   }
   Ok(())
@@ -129,6 +139,7 @@ fn png_bits() -> Result<()> {
         bits,
         ..Default::default()
       },
+      false,
     )?;
   }
   Ok(())
@@ -145,6 +156,7 @@ fn jpeg_bits() -> Result<()> {
         bits,
         ..Default::default()
       },
+      false,
     )?;
   }
   Ok(())
@@ -161,6 +173,7 @@ fn png_wrong_depth_and_lsbs() {
       bits: 7,
       ..Default::default()
     },
+    false,
   );
   assert!(result.is_err());
 }
@@ -176,13 +189,14 @@ fn jpeg_wrong_depth_and_lsbs() {
       bits: 2,
       ..Default::default()
     },
+    false,
   );
   assert!(result.is_err());
 }
 #[test]
 fn png_full() -> Result<()> {
   let total_size = 128 * 128 * 3 - 32;
-  e2e("png", (128, 128), total_size >> 3, ExtraArgs::default())?;
+  e2e("png", (128, 128), total_size >> 3, ExtraArgs::default(), false)?;
   e2e(
     "png",
     (128, 128),
@@ -191,6 +205,7 @@ fn png_full() -> Result<()> {
       bits: 8,
       ..Default::default()
     },
+    false,
   )?;
   Ok(())
 }
@@ -198,7 +213,7 @@ fn png_full() -> Result<()> {
 #[test]
 fn jpeg_full() -> Result<()> {
   let total_size = 128 * 128 * 3 - 32;
-  e2e("jpg", (128, 128), total_size >> 3, ExtraArgs::default())?;
+  e2e("jpg", (128, 128), total_size >> 3, ExtraArgs::default(), false)?;
   e2e(
     "jpg",
     (128, 128),
@@ -207,6 +222,7 @@ fn jpeg_full() -> Result<()> {
       bits: 8,
       ..Default::default()
     },
+    false,
   )?;
   Ok(())
 }
@@ -222,7 +238,23 @@ fn jpeg_components() -> Result<()> {
         jpeg_comp: Some(comp),
         ..Default::default()
       },
+      false,
     )?;
   }
+  Ok(())
+}
+
+#[test]
+fn jpeg_selective() -> Result<()> {
+  e2e(
+    "jpg",
+    (128, 128),
+    128,
+    ExtraArgs {
+      selective: true,
+      ..Default::default()
+    },
+    true,
+  )?;
   Ok(())
 }
